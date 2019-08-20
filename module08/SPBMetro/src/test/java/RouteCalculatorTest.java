66166027b60744b30b06775cb6448d33f8dd13ef
AlekseyKhan <AlekseyKhankhatov@gmail.com>
2019-08-20 11:37:18 +0800

    RouteCalculatorTest.java complete, delete invalid lines in RouteCalculator.java

--- module08/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ module08/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -17,8 +17,8 @@ public class RouteCalculatorTest extends TestCase {
     /**
      * инищиализация входных данных для тестирования
      *
-     *                   север ---      начало
-     *                  |        /           |
+     *   остров --- полюс/север -             начало
+     *                          |             |
      * ст1  -  ст2/запад  -  ст3/восток/централ  -  ст4
      *                 |        |            |
      *                 - юг ----         конец
@@ -32,9 +32,12 @@ public class RouteCalculatorTest extends TestCase {
         Line line1 = new Line(1, "Главная линия");
         Line line2 = new Line(2, "Круговое");
         Line line3 = new Line(3, "Вертикальная");
+        Line line4 = new Line(4, "Полюс-Север");
 
         stationIndex.addLine(line1);
         stationIndex.addLine(line2);
+        stationIndex.addLine(line3);
+        stationIndex.addLine(line4);
 
         addStations(
                 stationIndex,
@@ -49,9 +52,9 @@ public class RouteCalculatorTest extends TestCase {
                 stationIndex,
                 line2,
                 new Station("Север", line2),
-                new Station("Запад", line2),
+                new Station("Восток", line2),
                 new Station("Юг", line2),
-                new Station("Восток", line2)
+                new Station("Запад", line2)
         );
 
         addStations(
@@ -60,6 +63,13 @@ public class RouteCalculatorTest extends TestCase {
                 new Station("Начало", line3),
                 new Station("Централ", line3),
                 new Station("Конец", line3)
+        );
+
+        addStations(
+                stationIndex,
+                line4,
+                new Station("Остров", line4),
+                new Station("Полюс", line4)
         );
 
         List<Station> connectionOnCentral = Arrays.asList(
@@ -73,8 +83,14 @@ public class RouteCalculatorTest extends TestCase {
                 stationIndex.getStation("Запад")
         );
 
+        List<Station> connectionPoleNord = Arrays.asList(
+                stationIndex.getStation("Полюс"),
+                stationIndex.getStation("Север")
+        );
+
         stationIndex.addConnection(connectionOnCentral);
         stationIndex.addConnection(connectionOnMain);
+        stationIndex.addConnection(connectionPoleNord);
 
     }
 
@@ -83,14 +99,7 @@ public class RouteCalculatorTest extends TestCase {
      * без пересадок, на одной ветке
      */
     public void testCalculateDuration_onSameLine() {
-        double actual = RouteCalculator.calculateDuration(
-                Arrays.asList(
-                        stationIndex.getStation("Ст1"),
-                        stationIndex.getStation("Ст2"),
-                        stationIndex.getStation("Ст3"),
-                        stationIndex.getStation("Ст4")
-                )
-        );
+        double actual = RouteCalculator.calculateDuration(buildRoute("Ст1 -> Ст2 -> Ст3 -> Ст4"));
         //ст1 - 2.5 - ст2 - 2.5 - ст3 - 2.5 - ст4
         double expected = 7.5;
 
@@ -102,14 +111,7 @@ public class RouteCalculatorTest extends TestCase {
      * с одним переходом
      */
     public void testCalculateDuration_withOneJump() {
-        double actual = RouteCalculator.calculateDuration(
-                Arrays.asList(
-                        stationIndex.getStation("Начало"),
-                        stationIndex.getStation("Централ"),
-                        stationIndex.getStation("Восток"),
-                        stationIndex.getStation("Юг")
-                )
-        );
+        double actual = RouteCalculator.calculateDuration(buildRoute("Начало -> Централ -> Восток -> Юг"));
         //начало - 2.5 - централ - 3.5 - восток - 2.5 - юг
         double expected = 8.5;
 
@@ -124,15 +126,10 @@ public class RouteCalculatorTest extends TestCase {
 
         routeCalculator = new RouteCalculator(stationIndex);
         List<Station> actual = routeCalculator.getShortestRoute(
-                stationIndex.getStation("Ст1"),
-                stationIndex.getStation("Ст4")
+                stationIndex.getStation("Север"),
+                stationIndex.getStation("Восток")
         );
-        List<Station> expected = Arrays.asList(
-                stationIndex.getStation("Ст1"),
-                stationIndex.getStation("Ст2"),
-                stationIndex.getStation("Ст3"),
-                stationIndex.getStation("Ст4")
-        );
+        List<Station> expected = buildRoute("Север -> Восток");
 
         assertEquals(expected, actual);
     }
@@ -145,23 +142,27 @@ public class RouteCalculatorTest extends TestCase {
 
         routeCalculator = new RouteCalculator(stationIndex);
         List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Начало"), stationIndex.getStation("Юг"));
-        List<Station> expected = new ArrayList<>(Arrays.asList(
-                stationIndex.getStation("Начало"),
-                stationIndex.getStation("Централ"),
-                stationIndex.getStation("Восток"),
-                stationIndex.getStation("Юг")
-        ));
+        List<Station> expected = buildRoute("Начало -> Централ -> Восток -> Юг");
 
         assertEquals(expected, actual);
     }
 
-    @Override
-    protected void tearDown() throws Exception {
+    /**
+     * Проверка работы метода {@link RouteCalculator#getShortestRoute(Station, Station)}
+     * с двумя пересадками
+     */
+    public void testGetShortestRoute_withTwoJump() {
 
+        routeCalculator = new RouteCalculator(stationIndex);
+        List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Начало"), stationIndex.getStation("Остров"));
+        List<Station> expected = buildRoute("Начало -> Централ -> Восток -> Север -> Полюс -> Остров");
+
+        assertEquals(expected, actual);
     }
 
     /**
      * Метод заносит в stationIndex и line указанные станции
+     *
      * @param stationIndex
      * @param line
      * @param stations
@@ -175,6 +176,29 @@ public class RouteCalculatorTest extends TestCase {
         } else {
             System.out.println("Станции отсутствуют");
         }
+    }
+
+    /**
+     * Метод строит маршрут по входящей строке, которая разбивается на маршруты
+     *
+     * @param input
+     * @return
+     */
+    private List<Station> buildRoute(String input) {
+        List<Station> stations = new ArrayList<>();
+
+        Arrays.stream(input.split("->")).forEach(station -> {
+            Station searchStation = stationIndex.getStation(station.trim());
+            if (searchStation != null) {
+                stations.add(searchStation);
+            } else {
+                System.out.printf("При создании маршрута '%s' возникла ошибка", input);
+                stations.clear();
+                return;
+            }
+        });
+
+        return stations;
     }
 
 }
