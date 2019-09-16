ed980674d32961aebb8b883648079889a9ef3584
AlekseyKhan <AlekseyKhankhatov@gmail.com>
2019-09-16 10:22:04 +0800

    Started module 10, hw1

---
+++ module10/myjdbc/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/skillbox?serverTimezone=UTC";
+        String user = "root";
+        String password = "Root";
+        String home1Query ="SELECT (SELECT name FROM courses WHERE id=course_id) AS course" +
+                        "   ,MONTH(subscription_date) AS monthNum" +
+                        "   ,MONTHNAME(subscription_date) as monthName" +
+                        "   ,COUNT(*) AS purchaseNum" +
+                        "   FROM subscriptions" +
+                        "   GROUP by course, monthNum" +
+                        "   ORDER by course, monthNum" +
+                        ";";
+
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+
+        ){
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(home1Query);
+
+            while (resultSet.next() ) {
+                String courseName = resultSet.getString("course");
+                String monthName = resultSet.getString("monthName");
+                int purchaseNum = resultSet.getInt("purchaseNum");
+                System.out.println(courseName + "\t\t" + monthName + "\t\t" + purchaseNum);
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
